b894ae169bcf866458fea1503d40c46a73dc1a52
toosyou <toosyou.tw@gmail.com>
2019-11-09 00:21:29 +0800

    make denoise cup-paralleled

--- denoise.py
+++ denoise.py
@@ -2,6 +2,8 @@ import numpy as np
 import pywt
 from scipy.signal import medfilt
 
+import multiprocessing as mp
+
 def wavelet_threshold(data, wavelet='sym8', noiseSigma=14):
     levels = int(np.floor(np.log2(data.shape[0])))
     WC = pywt.wavedec(data,wavelet,level=levels)
@@ -14,10 +16,26 @@ def baseline_wander_removal(data):
     baseline = medfilt(baseline, 601)
     return data - baseline
 
+def _denoise_mp(signal):
+    return baseline_wander_removal(wavelet_threshold(signal))
+
 def denoise(ecg_data, number_channels=None):
     number_channels = ecg_data.shape[0] if number_channels is None else number_channels
-    for i in range(number_channels): # number of channel
-        ecg_data[i] = wavelet_threshold(ecg_data[i])
-        ecg_data[i] = baseline_wander_removal(ecg_data[i])
+
+    with mp.Pool(processes=number_channels) as workers:
+        results = list()
+
+        for i in range(number_channels):
+            results.append(workers.apply_async(_denoise_mp, (ecg_data[i], )))
+
+        workers.close()
+        workers.join()
+
+        for i, result in enumerate(results):
+            ecg_data[i] = result.get()
+
+    # for i in range(number_channels): # number of channel
+    #     ecg_data[i] = wavelet_threshold(ecg_data[i])
+    #     ecg_data[i] = baseline_wander_removal(ecg_data[i])
 
     return ecg_data
